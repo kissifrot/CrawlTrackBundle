9e574d3c26b2adb9cb20a59cbbbfbadd677077f1
kissifrot <kissifrot@gmail.com>
2015-10-20 23:21:43 +0200

    Added missing form type for CrawlerIPData

---
+++ Form/CrawlerIPDataType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace WebDL\CrawltrackBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CrawlerIPDataType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('ipAddress')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'WebDL\CrawltrackBundle\Entity\CrawlerIPData'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'webdl_crawltrackbundle_crawler_ip_data';
+    }
+}
