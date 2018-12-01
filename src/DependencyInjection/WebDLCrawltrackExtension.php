7dc420eb5e5d5637dcda9a2ba832d58bb4fdcf65
Philippe Villiers <kissifrot@gmail.com>
2018-12-01 18:02:29 +0100

    Fix PSR-4 and update version dependencies

--- src/DependencyInjection/WebDLCrawltrackExtension.php
+++ src/DependencyInjection/WebDLCrawltrackExtension.php
@@ -22,7 +22,7 @@ class WebDLCrawltrackExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
 
         if(isset($config['db_table_prefix'])) {
